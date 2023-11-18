adf1b5ca66d98e25ca7132178518ec7f4cd95699
palavarapuakhil <65067937+palavarapuakhil@users.noreply.github.com>
2023-11-18 12:11:01 +0530

    Create program 4.py

---
+++ program 4.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+np.random.seed(42)
+
+num_students = 100
+
+courses = ['Math', 'Physics', 'History', 'Biology']
+
+data = {
+    'Student_ID': np.arange(1, num_students + 1),
+    'Course': np.random.choice(courses, num_students),
+    'Score': np.random.randint(50, 100, num_students),
+    'Hours_Studied': np.random.uniform(5, 30, num_students)
+}
+
+student_data = pd.DataFrame(data)
+
+print(student_data.head())
